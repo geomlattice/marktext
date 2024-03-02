142dfeda74ca1223cccfdc45e7bcbd0080b0484d
Keanu Clark <markov.particle@gmail.com>
2024-03-02 01:20:18 -0500

    added intro chunk of Houlgate's Hegel on Being

---
+++ book_introductions/Hegel_on_Being/display_angel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+./display_demon.sh preface.md ./notes/preface.md
+./display_demon.sh pp3.md ./notes/pp3.md
+./display_demon.sh pp4.md ./notes/pp4.md
+./display_demon.sh pp5.md ./notes/pp5.md
+./display_demon.sh pp6.md ./notes/pp6.md
+./display_demon.sh pp7.md ./notes/pp7.md
+./display_demon.sh pp8.md ./notes/pp8.md
+./display_demon.sh pp9.md ./notes/pp9.md
+./display_demon.sh pp10.md ./notes/pp10.md
+./display_demon.sh pp11.md ./notes/pp11.md
+./display_demon.sh pp12.md ./notes/pp12.md
+./display_demon.sh pp13.md ./notes/pp13.md
+./display_demon.sh pp14.md ./notes/pp14.md
+./display_demon.sh pp15.md ./notes/pp15.md
+./display_demon.sh pp16.md ./notes/pp16.md
+./display_demon.sh pp17.md ./notes/pp17.md
