c1f475d84f114394027d9f1a512f49e949c757a4
Keanu Clark <markov.particle@gmail.com>
2024-03-03 02:01:33 -0500

    added primer for I Samkhya as Enumeration. This is very long and will take many sessions of editing due to lists and diagrams

---
+++ research/samkhya/larson_introduction_to_samkhya/philosophy_of_samkhya/I_Samkhya_as_Enumeration/display_angel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./display_demon.sh pp49.md ./notes/pp49.md
+./display_demon.sh pp50.md ./notes/pp50.md
+./display_demon.sh pp51.md ./notes/pp51.md
+./display_demon.sh pp52.md ./notes/pp52.md
+./display_demon.sh pp53.md ./notes/pp53.md
