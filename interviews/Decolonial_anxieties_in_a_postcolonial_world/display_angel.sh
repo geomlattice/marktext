cca05e90a31ce75124d7ba0a00ed7dbe23cc4f27
Keanu Clark <markov.particle@gmail.com>
2024-03-03 11:48:20 -0500

    citation expansion yeilded interview with Mbembe 2022

---
+++ interviews/Decolonial_anxieties_in_a_postcolonial_world/display_angel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./display_demon.sh pp128.md ./notes/pp128.md
+./display_demon.sh pp129.md ./notes/pp129.md
+./display_demon.sh pp130.md ./notes/pp130.md
+./display_demon.sh pp131.md ./notes/pp131.md
+./display_demon.sh pp132.md ./notes/pp132.md
+./display_demon.sh pp133.md ./notes/pp133.md
+./display_demon.sh pp134.md ./notes/pp134.md
+./display_demon.sh pp135.md ./notes/pp135.md
