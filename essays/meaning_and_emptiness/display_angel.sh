715b6aa196d7a512b6de33aded0ec8017fb718c8
Keanu Clark <markov.particle@gmail.com>
2024-03-04 03:11:11 -0500

    added essay from Esoteric Lacan on Ibn Arabi

---
+++ essays/meaning_and_emptiness/display_angel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./display_demon.sh pp215.md ./notes/pp215.md
+./display_demon.sh pp216.md ./notes/pp216.md
+./display_demon.sh pp217.md ./notes/pp217.md
+./display_demon.sh pp218.md ./notes/pp218.md
+./display_demon.sh pp219.md ./notes/pp219.md
+./display_demon.sh pp220.md ./notes/pp220.md
+./display_demon.sh pp221.md ./notes/pp221.md
+./display_demon.sh pp222.md ./notes/pp222.md
+./display_demon.sh pp223.md ./notes/pp223.md
+./display_demon.sh pp224.md ./notes/pp224.md
+./display_demon.sh pp225.md ./notes/pp225.md
+./display_demon.sh pp226.md ./notes/pp226.md
+./display_demon.sh pp227.md ./notes/pp227.md
+./display_demon.sh pp228.md ./notes/pp228.md
+./display_demon.sh pp229.md ./notes/pp229.md
