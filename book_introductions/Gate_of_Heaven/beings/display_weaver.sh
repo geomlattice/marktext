f0f76ca1f1b9ce74a3989679b9026ca15c4eb489
Keanu Clark <markov.particle@gmail.com>
2024-03-06 11:16:37 -0500

    added subset of introduction of Gate of Heavan Translation

---
+++ book_introductions/Gate_of_Heaven/beings/display_weaver.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./display_mover.sh ../pages/pp23.md ../notes/pp23.md
+./display_mover.sh ../pages/pp24.md ../notes/pp24.md
+./display_mover.sh ../pages/pp25.md ../notes/pp25.md
+./display_mover.sh ../pages/pp26.md ../notes/pp26.md
+./display_mover.sh ../pages/pp27.md ../notes/pp27.md
+./display_mover.sh ../pages/pp28.md ../notes/pp28.md
+./display_mover.sh ../pages/pp29.md ../notes/pp29.md
+./display_mover.sh ../pages/pp30.md ../notes/pp30.md
