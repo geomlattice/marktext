0eb22d01f5c44659e8b36acfcba0ed68d641b3c4
Keanu Clark <markov.particle@gmail.com>
2024-03-08 12:14:04 -0500

    added Essay on Aristotle and Ps-Dionysius

---
+++ essays/Aristotle_and_Pseudo_Dionysius_in_Syriac_Tradition/beings/display_weaver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./display_mover.sh ../pages/pp25.md ../notes/pp25.md
+./display_mover.sh ../pages/pp26.md ../notes/pp26.md
+./display_mover.sh ../pages/pp27.md ../notes/pp27.md
+./display_mover.sh ../pages/pp28.md ../notes/pp28.md
+./display_mover.sh ../pages/pp29.md ../notes/pp29.md
+./display_mover.sh ../pages/pp30.md ../notes/pp30.md
+./display_mover.sh ../pages/pp31.md ../notes/pp31.md
+./display_mover.sh ../pages/pp32.md ../notes/pp32.md
+./display_mover.sh ../pages/pp33.md ../notes/pp33.md
+./display_mover.sh ../pages/pp34.md ../notes/pp34.md
+./display_mover.sh ../pages/pp35.md ../notes/pp35.md
