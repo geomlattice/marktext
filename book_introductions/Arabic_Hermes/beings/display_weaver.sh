2d47f3d1e953f64fa5c8397f49061438bf6ff210
Keanu Clark <markov.particle@gmail.com>
2024-03-13 14:40:59 -0400

    added the Arabic Hermes book introduction

---
+++ book_introductions/Arabic_Hermes/beings/display_weaver.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./display_mover.sh ../pages/pp3.md ../notes/pp3.md
+./display_mover.sh ../pages/pp4.md ../notes/pp4.md
+./display_mover.sh ../pages/pp5.md ../notes/pp5.md
+./display_mover.sh ../pages/pp6.md ../notes/pp6.md
+./display_mover.sh ../pages/pp7.md ../notes/pp7.md
+./display_mover.sh ../pages/pp8.md ../notes/pp8.md
+./display_mover.sh ../pages/pp9.md ../notes/pp9.md
+./display_mover.sh ../pages/pp10.md ../notes/pp10.md
+./display_mover.sh ../pages/pp11.md ../notes/pp11.md
+./display_mover.sh ../pages/pp12.md ../notes/pp12.md
+./display_mover.sh ../pages/pp13.md ../notes/pp13.md
+./display_mover.sh ../pages/pp14.md ../notes/pp14.md
+./display_mover.sh ../pages/pp15.md ../notes/pp15.md
+./display_mover.sh ../pages/pp16.md ../notes/pp16.md
+./display_mover.sh ../pages/pp17.md ../notes/pp17.md
+./display_mover.sh ../pages/pp18.md ../notes/pp18.md
+./display_mover.sh ../pages/pp19.md ../notes/pp19.md
+./display_mover.sh ../pages/pp20.md ../notes/pp20.md
+./display_mover.sh ../pages/pp21.md ../notes/pp21.md
+./display_mover.sh ../pages/pp22.md ../notes/pp22.md
